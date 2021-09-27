93d4e0bf100d35b10e9436e0fd2b9c873700c6af
jionchu <fn0809@gmail.com>
2021-09-27 15:48:10 +0900

    :sparkles: feat: 스플래시 화면 추가

---
+++ app/src/main/java/com/taka/taka/ui/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.taka.taka.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.taka.taka.MainActivity
+import com.taka.taka.R
+import kotlin.concurrent.thread
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        thread {
+            Thread.sleep(1000)
+
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
