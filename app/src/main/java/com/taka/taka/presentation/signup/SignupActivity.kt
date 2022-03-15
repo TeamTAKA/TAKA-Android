bb7a04b101d31c87a3cb2f038150b87976ce7763
jionchu <fn0809@gmail.com>
2022-03-16 02:30:03 +0900

    ✨ feat: 회원가입 id 중복확인 기능 구현

--- app/src/main/java/com/taka/taka/presentation/signup/SignupActivity.kt
+++ app/src/main/java/com/taka/taka/presentation/signup/SignupActivity.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
+import androidx.activity.viewModels
+import androidx.core.widget.doAfterTextChanged
 import com.taka.taka.presentation.MainActivity
 import com.taka.taka.R
 import com.taka.taka.databinding.ActivitySignupBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignupActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: SignupViewModel
+    private val viewModel: SignupViewModel by viewModels()
     private val binding: ActivitySignupBinding by lazy {
         ActivitySignupBinding.inflate(
             layoutInflater
@@ -20,19 +23,32 @@ class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
-
-        viewModel = ViewModelProvider(
-            this,
-            ViewModelProvider.NewInstanceFactory()
-        ).get(SignupViewModel::class.java)
+        setContentView(binding.root)
 
         binding.signupTvLogin.setOnClickListener { finish() }
+        binding.signupTvDuplicate.setOnClickListener {
+            viewModel.checkId(binding.signupEtId.text.toString().trim())
+        }
         binding.signupTvSignup.setOnClickListener {
             viewModel.signup(
                 binding.signupEtId.text.toString(),
                 binding.signupEtPwd.text.toString()
             )
+        }
+        binding.signupEtId.doAfterTextChanged { viewModel.setIdUnchecked() }
+
+        viewModel.isChecked.observe(this) { isChecked ->
+            if (isChecked) {
+                binding.signupTvDuplicate.apply {
+                    text = getString(R.string.signup_duplicate_checked)
+                    setTextColor(resources.getColor(R.color.blue))
+                }
+            } else {
+                binding.signupTvDuplicate.apply {
+                    text = getString(R.string.signup_duplicate_check)
+                    setTextColor(resources.getColor(R.color.orange_red))
+                }
+            }
         }
 
         viewModel.signupSuccess.observe(this) {
--- app/src/main/java/com/taka/taka/presentation/signup/SignupViewModel.kt
+++ app/src/main/java/com/taka/taka/presentation/signup/SignupViewModel.kt
@@ -3,15 +3,41 @@ package com.taka.taka.presentation.signup
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.taka.taka.domain.repository.TicketRepository
+import com.taka.taka.domain.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SignupViewModel : ViewModel() {
+@HiltViewModel
+class SignupViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+    private val ticketRepository: TicketRepository
+) : ViewModel() {
 
     private val _state: MutableLiveData<String> = MutableLiveData()
     val state: LiveData<String>
         get() = _state
+    private val _isChecked: MutableLiveData<Boolean> = MutableLiveData()
+    val isChecked: LiveData<Boolean> get() = _isChecked
     private val _signupSuccess: MutableLiveData<Boolean> = MutableLiveData()
     val signupSuccess: LiveData<Boolean>
         get() = _signupSuccess
+
+    fun checkId(id: String) {
+        viewModelScope.launch {
+            try {
+                _isChecked.value = userRepository.checkId(id).success
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun setIdUnchecked() {
+        _isChecked.value = false
+    }
 
     fun signup(id: String, pwd: String) {
         // id 혹은 pwd가 빈칸인 경우 회원가 불가
